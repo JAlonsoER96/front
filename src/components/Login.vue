3c6346d1bbac499376c08c35c6da0a15c8f1c619
jose.ekvc <jose.ekvc@gmail.com>
2020-07-15 12:21:52 -0500

    Meto do Ingreso al sistema

---
+++ src/components/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 lg5 x14>
+      <v-card>
+        <v-toolbar dark>
+          <v-toolbar-title>Acceso al sistema</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-text-field autofocus color="accent" label="Email" required v-model="email"></v-text-field>
+          <v-text-field color="accent" label="Contraseña" type="password" v-model="password"></v-text-field>
+        </v-card-text>
+        <v-card-actions class="px-3 pb-3">
+          <v-flex text-xs-center>
+            <v-btn color="primary" @click="ingresar">Ingresar</v-btn>
+          </v-flex>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    email: "",
+    password: ""
+  }),
+  methods: {
+    ingresar() {
+      axios
+        .post("usuario/login", { email: this.email, password: this.password })
+        .then(function(response) {
+          console.log(response.data.user);
+          console.log(response.data.tokenReturn);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Categoria from '../components/Categoria.vue'
+import Login from '../components/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,11 @@ Vue.use(VueRouter)
       path: '/categorias',
       name: 'Categoria',
       component: Categoria
+    },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 
